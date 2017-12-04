d9e780da2c4fc12af4ebcce0bcbb201b57ced2e7
pendragon <arthuryaucs@gmail.com>
2017-12-04 21:48:15 +0800

    1. adjust the skybox distance from eye to skybox

--- SkyBoxFragmentShader.glsl
+++ SkyBoxFragmentShader.glsl
@@ -22,7 +22,7 @@ void main(){
     if(fogFlag == 1){
       //float distance = length((viewMatrix * RotationMatrix * TransformMatrix * ScalingMatrix * v));
       float visibility = 1;
-      float distance_of_eye_to_skybox = 55.0f;
+      float distance_of_eye_to_skybox = 85.0f;
       visibility = exp(-pow((distance_of_eye_to_skybox * fogDensity), fogGradient));
       visibility = clamp(visibility, 0, 1);
       color = mix(fogColor, color, visibility);
